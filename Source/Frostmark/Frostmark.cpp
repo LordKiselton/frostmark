8c5268ba0f7c39cb9a5ea0aca74b6f7eefd9a82a
Kiselton <ad.kopyrin@gmail.com>
2025-10-25 13:48:56 +0300

    frostmark created

---
+++ Source/Frostmark/Frostmark.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Frostmark.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Frostmark, "Frostmark" );
+
+DEFINE_LOG_CATEGORY(LogFrostmark)
+ 
